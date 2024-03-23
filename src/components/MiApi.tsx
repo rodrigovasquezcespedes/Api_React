8ad1c0cad00d9982f0dc31f590ebd51da4559207
rodrigo vasquez cespedes <rvasquez26@gmail.com>
2024-03-23 11:29:49 -0300

    modifica estructura url

--- src/components/MiApi.tsx
+++ src/components/MiApi.tsx
@@ -6,23 +6,24 @@ import SpinnerComponent from './SpinnerComponent'
 
 
 interface Props {
-    searchInput: string;
+    searchInput: string
 }
 
 const MiApi: React.FC<Props> = ({ searchInput }) => {
-    const [cards, setCards] = useState<any[]>([]);
-    const [isLoading, setLoading] = useState(true);
+    const [cards, setCards] = useState<any[]>([])
+    const [isLoading, setLoading] = useState(true)
 
     const CardApi = async () => {
-        const apiKey = '2e7833fa3306fdbfeff831a6ede55741';
-        const hashKey = '61e8b6d2d1b850fe7b48907828e6fea1';
-        const apiUrl = 'https://gateway.marvel.com/v1/public/characters';
+        const apiKey = '2e7833fa3306fdbfeff831a6ede55741'
+        const hashKey = '61e8b6d2d1b850fe7b48907828e6fea1'
+        const apiUrl = 'https://gateway.marvel.com/v1/public/characters'
+        const limits='30'
 
         let url;
         if (searchInput !== '') {
-            url = `${apiUrl}?nameStartsWith=${searchInput}&ts=1&apikey=${apiKey}&hash=${hashKey}&limit=30`;
+            url = `${apiUrl}?nameStartsWith=${searchInput}&ts=1&apikey=${apiKey}&hash=${hashKey}&limit=${limits}`;
         } else {
-            url = `${apiUrl}?ts=1&apikey=${apiKey}&hash=${hashKey}&limit=30`;
+            url = `${apiUrl}?ts=1&apikey=${apiKey}&hash=${hashKey}&limit=${limits}`;
         }
         const response = await fetch(url);
         const data = await response.json();
